ad56dedbf26a7342096919a99311afaef6851893
codeurLicorne <Jumpnotion@gmail.com>
2022-11-26 13:13:02 -0500

    initl commit 8games*days

---
+++ Assets/unity-pong-tutorial-main/Assets/Scripts/ScoringZone.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ScoringZone : MonoBehaviour
+{
+    public UnityEvent scoreTrigger;
+    public UnityEvent scoreBonus;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null) {
+            scoreTrigger.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null)
+        {
+            scoreBonus.Invoke();
+        }
+    }
+
+}
